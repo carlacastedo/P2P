13936a1ac68494a8a9435da90448f98b085fd2d2
anton <anton@ANTON>
2021-11-20 18:04:38 +0100

    todos los metodos que faltaban

--- P2P/src/p2p/BaseDatos.java
+++ P2P/src/p2p/BaseDatos.java
@@ -38,7 +38,7 @@ public class BaseDatos {
                     + configuracion.getProperty("puerto") + "/"
                     + configuracion.getProperty("baseDatos"),
                     usuario);
-
+            
         } catch (FileNotFoundException f) {
             System.out.println(f.getMessage());
         } catch (IOException | java.sql.SQLException i) {
@@ -50,32 +50,34 @@ public class BaseDatos {
         Connection con = this.conexion;
         PreparedStatement stmUsuarios = null;
         ResultSet rsUsuarios;
-        String consulta = "select * from usuarios";
+        String consulta = "select nombre from usuarios";
         try {
             stmUsuarios = con.prepareStatement(consulta);
             rsUsuarios = stmUsuarios.executeQuery();
             while (rsUsuarios.next()) {
-                System.out.println(rsUsuarios.getString("nombre") + rsUsuarios.getString("contraseña"));
+                System.out.println(rsUsuarios.getString("nombre"));
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         } finally {
             try {
-                if(stmUsuarios!=null) stmUsuarios.close();
+                if (stmUsuarios != null) {
+                    stmUsuarios.close();
+                }
             } catch (SQLException ex) {
                 System.out.println(ex.getMessage());
             }
         }
     }
 
-    public void conseguirAmigos(String usuario) {
+    public void consultarAmigos(String usuario) {
         Connection con = this.conexion;
 
         String consulta = "select solicitante from solicitar_amistad "
-                            +"where solicitado=? and estado='aceptado' "
-                            +"UNION "
-                            +"select solicitado from solicitar_amistad "
-                            +"where solicitante=? and estado='aceptado' ";
+                + "where solicitado=? and estado='aceptado' "
+                + "UNION "
+                + "select solicitado from solicitar_amistad "
+                + "where solicitante=? and estado='aceptado' ";
 
         try (PreparedStatement stmUsuario = con.prepareStatement(consulta)) {
             stmUsuario.setString(1, usuario);
@@ -88,5 +90,111 @@ public class BaseDatos {
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
+    }
+    
+    public void consultarSolicitudes(String solicitado) {
+        Connection con = this.conexion;
+
+        String consulta = "select solicitante from solicitar_amistad "
+                + "where solicitado=? and estado='pendiente'";
+        try (PreparedStatement stmUsuario = con.prepareStatement(consulta)) {
+            stmUsuario.setString(1, solicitado);
+            try (ResultSet rsUsuario = stmUsuario.executeQuery()) {
+                while (rsUsuario.next()) {
+                    System.out.println(rsUsuario.getString("solicitante"));
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println(consulta + "\n" + e.getMessage());
+        }
+    }
+
+    public void insertarUsuario(String usuario, String contraseña) {
+        Connection con = this.conexion;
+
+        String consulta = "insert into usuarios(nombre, contraseña) "
+                + "values(?, ?)";
+        try (PreparedStatement stmUsuario = con.prepareStatement(consulta)) {
+            stmUsuario.setString(1, usuario);
+            stmUsuario.setString(2, contraseña);
+            stmUsuario.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void enviarSolicitud(String solicitante, String solicitado) {
+        Connection con = this.conexion;
+
+        String consulta = "insert into solicitar_amistad(solicitante, solicitado, estado) "
+                + "values(?, ?, 'pendiente')";
+
+        try (PreparedStatement stmSolAmis = con.prepareStatement(consulta)) {
+            stmSolAmis.setString(1, solicitante);
+            stmSolAmis.setString(2, solicitado);
+            stmSolAmis.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void aceptarSolicitud(String solicitante, String solicitado) {
+        Connection con = this.conexion;
+
+        String modificacion = "update solicitar_amistad set estado='aceptado' where solicitante=? and"
+                + " solicitado=?";
+
+        try (PreparedStatement stmSolAmis = con.prepareStatement(modificacion)) {
+            stmSolAmis.setString(1, solicitante);
+            stmSolAmis.setString(2, solicitado);
+            stmSolAmis.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void denegarSolicitud(String solicitante, String solicitado) {
+        Connection con = this.conexion;
+
+        String borrado = "delete from solicitar_amistad where solicitante=? and"
+                + " solicitado=? and estado='pendiente'";
+
+        try (PreparedStatement stmSolAmis = con.prepareStatement(borrado)) {
+            stmSolAmis.setString(1, solicitante);
+            stmSolAmis.setString(2, solicitado);
+            stmSolAmis.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void eliminarAmigo(String solicitante, String solicitado) {
+        Connection con = this.conexion;
+
+        String insercion = "delete from solicitar_amistad where solicitante=? and"
+                + " solicitado=? and estado='aceptado'";
+
+        try (PreparedStatement stmSolAmis = con.prepareStatement(insercion)) {
+            stmSolAmis.setString(1, solicitante);
+            stmSolAmis.setString(2, solicitado);
+            stmSolAmis.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public void modificarContraseña(String usuario, String contraseña){
+        Connection con = this.conexion;
+
+        String modificacion = "update usuarios set contraseña=? where nombre=?";
+
+        try (PreparedStatement stmSolAmis = con.prepareStatement(modificacion)) {
+            stmSolAmis.setString(1, contraseña);
+            stmSolAmis.setString(2, usuario);
+            stmSolAmis.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        
     }
 }
