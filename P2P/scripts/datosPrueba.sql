4e0fb715fec1e61b145772a662efd4fb8c61af78
carlacastedo <57439309+carlacastedo@users.noreply.github.com>
2021-11-20 11:20:26 +0100

    faltan ;

--- P2P/scripts/datosPrueba.sql
+++ P2P/scripts/datosPrueba.sql
@@ -3,6 +3,6 @@ insert into usuarios(nombre,contraseña) values('anton','1234');
 insert into usuarios(nombre,contraseña) values('maria','5678');
 insert into usuarios(nombre,contraseña) values('juan','5678');
 
-insert into solicitar_amistad(solicitante, solicitado,estado) values('carla','anton','aceptado')
+insert into solicitar_amistad(solicitante, solicitado,estado) values('carla','anton','aceptado');
 insert into solicitar_amistad(solicitante, solicitado,estado) values('maria','carla','aceptado');
-insert into solicitar_amistad(solicitante, solicitado,estado) values('juan','carla','pendiente')
+insert into solicitar_amistad(solicitante, solicitado,estado) values('juan','carla','pendiente');
